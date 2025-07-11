7d1257c6f9a2035d61fad12e65a1ac522d24558c
Oliwier Gramala <olivier.gramala@gmail.com>
2025-07-11 08:15:43 +0200

    First 2, 13 to go

---
+++ GuessingNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿void game(int min, int max)
+{
+    Console.WriteLine($"Try to guess number between {min} and {max}:");
+    Random rnd = new Random();
+    int randomNumber = rnd.Next(min, max);
+    int userInput = 0;
+    while (true)
+    {
+        try
+        {
+            Console.WriteLine("Try guess!");
+            userInput = Int32.Parse(Console.ReadLine());
+            if (userInput == randomNumber)
+            {
+                Console.WriteLine($"You are right! The number is {randomNumber}");
+                break;
+            }
+                
+        }catch (Exception) {
+            Console.WriteLine("Invalid input");
+        }
+    }
+}
+
+int min = 0;
+int max = 100;
+
+game(min,max);
---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Welcome \nChose option:\n0) Sum\n1) Sub\n2) Div\n3) Mul\n4) Pow");
+int userInput = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("A:");
+double a = double.Parse(Console.ReadLine());
+
+Console.WriteLine("B:");
+double b = double.Parse(Console.ReadLine());
+
+double result = 0;
+
+switch (userInput)
+{
+    case 0:
+        result = a + b;
+        break;
+    case 1:
+        result = a - b; 
+        break;
+    case 2:
+        result = a / b; 
+        break;
+    case 3:
+        result = a * b;
+        break;
+    case 4:
+        for (int i = 1; i<b;i++)
+        {
+            result += a * a;
+        }
+        break;
+    default: break;
+}
+
+Console.WriteLine(result);
